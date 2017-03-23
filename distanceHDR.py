415eba67d0b948ce3bbfe601f7e07681439ad82e
Krishnan Raghavan <krm9c@mst.edu>
2017-03-23 12:53:01 -0500

    First commit for the distance covariance dimension reduction

---
+++ distanceHDR.py
@@ -0,0 +1,13 @@
+"""
+Testing For Paper-II
+"""
+# import all the required Libraries
+import math
+import numpy as np
+import time, os, sys
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    print "hello let us start working on the second paper"
+    
